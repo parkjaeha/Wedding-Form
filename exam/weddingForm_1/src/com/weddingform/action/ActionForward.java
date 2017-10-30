825a48326644fb4e7420445e07ba38d70ed33a4e
LeeEungjin <dmdwls0208@gmail.com>
2017-10-30 16:54:14 +0900

    weddingform_10/30

---
+++ exam/weddingForm_1/src/com/weddingform/action/ActionForward.java
@@ -0,0 +1,20 @@
+package com.weddingform.action;
+
+public class ActionForward {
+	private String path;
+	private boolean check;
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public boolean isCheck() {
+		return check;
+	}
+	public void setCheck(boolean check) {
+		this.check = check;
+	}
+	
+	
+}
