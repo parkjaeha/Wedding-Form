d9f0791a76a9e88a2b918b985d00b4e72f12f105
eunbi11 <33111500+eunbi11@users.noreply.github.com>
2017-11-09 16:00:16 +0900

    test2

--- SemiProject/WebContent/WEB-INF/view/common/commonAuthResponse.jsp
+++
@@ -1,9 +0,0 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
-<c:if test="${result eq true}">
-OK
-</c:if>
-
-<c:if test="${result eq false}">
-FAIL
-</c:if>
--- SemiProject/src/com/choa/sms/Config.java
+++
@@ -1,9 +0,0 @@
-package com.choa.sms;
-
-public final class Config {
-	public static final String appid = "weddingForm";
-	public static final String apikey = "key";
-	public static final String content = "인증번호: ";
-	public static final String sender = "01086505104";
-	public static final String receiver = "01086505104";
-}
